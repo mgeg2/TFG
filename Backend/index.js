f4066df4df5c7ffab527d93381402c3777e74f61
maria <mgeg2@gcloud.ua.es>
2025-07-07 14:44:23 +0200

    endpoint de prueba y cambiado para que inicie con nodemon

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, Munda!');
+  res.send('¡Hola, Mundo!');
 });
+app.get('/api', (req, res) => {
+  res.send('¡Hola, API!');});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
